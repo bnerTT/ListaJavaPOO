2bc666029fd3ba59eb204c74e5b3e9767fb7b5ae
Abner Torres <abnergama2512@gmail.com>
2024-02-06 20:36:09 -0300

    ft: questao 5

--- IntroducaoAoJava/Questao5/Q5.java
+++ IntroducaoAoJava/Questao5/Q5.java
@@ -1,8 +1,19 @@
 package IntroducaoAoJava.Questao5;
+import java.util.Scanner;
+
 
 public class Q5 {
     public static void main(String[] args){
-     int unidade, dezena, centena;
-        
+    double numeroModificado, numeroOriginal;
+    Scanner input = new Scanner(System.in);
+
+    numeroOriginal = input.nextInt();
+    numeroModificado = (numeroOriginal % 10) * 100;
+    numeroOriginal -= (numeroModificado/100);
+    numeroOriginal /= 10;
+    numeroModificado += numeroOriginal;
+    System.out.println("Número modificado: " + numeroModificado);
+    
+    input.close();
     }
 }
